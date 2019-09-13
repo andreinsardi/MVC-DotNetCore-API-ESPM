e6fd2da37073fe1ac36587b5c03f74fd18a9a818
André Insardi <andre.insardi@gmail.com>
2019-09-13 09:11:25 -0300

    cria exemplo de MVC com ODBC

---
+++ MVC-DotNetCore-API-ODBC/MVC-DotNetCore-API-ODBC/Models/AuhtoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MVC_DotNetCore_API_ODBC.Models
+{
+    public class AuhtoViewModel
+    {
+        public int AuthorID { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}   
---
+++ MVC-DotNetCore-API-ODBC/MVC-DotNetCore-API-ODBC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_DotNetCore_API_ODBC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
